2d72d31ad9b6459e271bdd5c64aca03d7d08fee1
German Gamboa <thegerman@Germans-MacBook-Pro.local>
2018-03-03 22:38:41 -0500

    testing vs code

--- main.js
+++ main.js
@@ -11,12 +11,16 @@ let taskManager = {
   incRender : () => {
     let x = taskManager.todo.length;
     let item = taskManager.todo[x - 1];
+    taskBox.innerHTML += `<li class="list-group-item d-flex justify-content-between"><span class="mr-5">${item}</span> <a href="" class="badge">Delete</a></li>`;
   },
   itemDelete : (e) => {
     let choice = e.target.innerHTML;
-    if(choice === 'Delete'){
+    if(choice === 'Delete'){ 
       e.target.parentElement.outerHTML = '';
       console.log(e);
+    },
+    printF: () => {
+      console.log('Hello World')
     }
   }
 }
